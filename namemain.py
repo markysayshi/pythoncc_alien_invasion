6729d50e6caeec0d5b2c48f9d8f934dbbea86dc8
Mark Garcia <markysayshi@gmail.com>
2024-07-01 13:47:30 -0500

    learning what if __name__ == '__main__' means

---
+++ namemain.py
@@ -0,0 +1,6 @@
+# namemain.py
+
+print(__name__, type(__name__))
+
+if __name__ == '__main__':
+    print("Nested code only runs in the top-level code environment.")
---
+++ test.py
@@ -0,0 +1,3 @@
+print("This is a test."[-3:])
+print("This is a test."[0:])
+print("This is a test."[-1:])
