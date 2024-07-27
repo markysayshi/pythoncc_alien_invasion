877718b3f95c9d325dd246dbc43a08eacf320147
mark <markysayshi@gmail.com>
2024-07-27 15:33:52 -0500

    finished going over code and tiy up to page 246

--- alien_invasion_test.py
+++ alien_invasion_test.py
@@ -11,9 +11,16 @@ bg_color = (230, 230, 230)
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
+###### FULLSCREEN, don't like this, but testing it out
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#screen_width = screen.get_rect().width
+#screen_height = screen.get_rect().height
+######
 pygame.display.set_caption("testing")
 
-image = pygame.image.load('images/ship.bmp')
+#image = pygame.image.load('images/ship.bmp')
+###### for try it yourself, page 246
+image = pygame.image.load('images/rocketship.bmp')
 #image = pygame.image.load('images/jackhead.bmp')
 
 ## this gets the rect of the image's surface
@@ -31,8 +38,10 @@ screen_rect = screen.get_rect()
 ## this sets the image rect position, midbottom
 ## to the screens rect position, midbottom
 ###### made this center for jackhead
-#rect.center = screen_rect.center
-rect.midbottom = screen_rect.midbottom
+rect.center = screen_rect.center
+###### This is the original
+#rect.midbottom = screen_rect.midbottom
+######
 ## an example of putting the image in the screen's center
 #rect.midbottom = screen_rect.center
 
@@ -45,14 +54,40 @@ print(rect)
 #print(screen_rect.midbottom)
 
 moving_right = False
+moving_left = False
+###### for try it yourself, page 246
+moving_up = False
+moving_down = False
 ## this works, if I wanted a function
 #def move_ship():
 #    if moving_right:
 #        rect.x += 1
 
+## ship speed settings
+ship_speed = 4.5
+## converting from int to float
+x = float(rect.x)
+###### for try it yourself, page 246
+y = float(rect.y)
+
+###### testing function, doesn't work, will mess with it later
+#def check_keydown_events(event):
+#    """Respond to key presses."""
+#    if event.key == pygame.K_RIGHT:
+#        moving_right = True
+#    elif event.key == pygame.K_LEFT:
+#        moving_left = True
+#
+#def check_keyup_events(event):
+#    """Respond to key releases."""
+#    if event.key == pygame.K_RIGHT:
+#        moving_right = False
+#    elif event.key == pygame.K_LEFT:
+#        moving_left = False
+######
+
 while True:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             sys.exit()
 
@@ -65,18 +100,49 @@ while True:
             ## shows everything about the event
             print(event)
             ######
+            #check_keydown_events(event)
             if event.key == pygame.K_RIGHT:
                 moving_right = True
+            elif event.key == pygame.K_LEFT:
+                moving_left = True
+            ###### for try it yourself, page 246
+            elif event.key == pygame.K_UP:
+                moving_up = True
+            elif event.key == pygame.K_DOWN:
+                moving_down = True
+            # exit game if q is pressed
+            elif event.key == pygame.K_q:
+                sys.exit()
 
         elif event.type == pygame.KEYUP:
+            #check_keyup_events(event)
             if event.key == pygame.K_RIGHT:
                 moving_right = False
+            elif event.key == pygame.K_LEFT:
+                moving_left = False
+            ###### for try it yourself, page 246
+            elif event.key == pygame.K_UP:
+                moving_up = False
+            elif event.key == pygame.K_DOWN:
+                moving_down = False
 
+    ## ship movement
     ## this works, if I wanted a function
     #move_ship()
     ## this works
-    if moving_right:
-        rect.x += 1
+    if moving_right and rect.right < screen_rect.right:
+        x += ship_speed
+    if moving_left and rect.left > 0:
+        x -= ship_speed
+    ###### for try it yourself, page 246
+    if moving_up and rect.top > 0:
+        y -= ship_speed
+    if moving_down and rect.bottom < screen_rect.bottom:
+        y += ship_speed
+
+    ## ship speed settings
+    rect.x = x
+    rect.y = y
 
     screen.fill((bg_color))
     screen.blit(image, rect)
