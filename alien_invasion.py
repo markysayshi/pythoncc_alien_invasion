d03119d30d54c697d8f77dc1b48bdd5ac5023d79
mark <markysayshi@gmail.com>
2025-03-20 10:05:17 -0500

    ready for tiy page 270

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,6 +89,20 @@ class AlienInvasion:
         # Keep uncommented to save memory, page 251.
         #print(len(self.bullets))
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+        #print(collisions)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
         self._check_fleet_edges()
